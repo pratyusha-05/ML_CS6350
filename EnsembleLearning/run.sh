2f537de3a4f5b2f72c6dfd4727dd212791c4dba4
pratyusha-05 <76260882+pratyusha-05@users.noreply.github.com>
2024-10-17 22:18:41 -0600

    Update run.sh

--- EnsembleLearning/run.sh
+++ EnsembleLearning/run.sh
@@ -11,3 +11,6 @@ python3 BiasVariance.py
 
 echo "Random Forest on Bank dataset"
 python3 RandomForest.py
+
+echo "Bias & Variance calculations of a single tree, bagged trees and random forest"
+python3 BiasVariance3comparision.py
