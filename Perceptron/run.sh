5ce75d39a3b3ec5d228ef304636c16ba8ef599a0
pratyusha-05 <76260882+pratyusha-05@users.noreply.github.com>
2024-11-01 13:55:36 -0600

    Create run.sh

---
+++ Perceptron/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "Result of Perceptron"
+python3 Perceptron.py
+
+echo "Results of voted perceptron"
+python3 VotedPerceptron.py
+
+echo "Results of average perceptron"
+python3 AveragePerceptron.py
