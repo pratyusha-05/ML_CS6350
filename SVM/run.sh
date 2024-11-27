77c29c549765b913e2cb0991a35c88ce89d500e8
pratyusha-05 <76260882+pratyusha-05@users.noreply.github.com>
2024-11-26 22:55:37 -0700

    Create run.sh

---
+++ SVM/run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+python3 SVMdual.py
+
+python3 SVMkernelPerceptron.py
+
+python3 SVMprimal.py
+
+python3 SVMprimal_b.py
+
+python3 SVMprimalDiff.py
+
+python3 SVMsupportVectors.py
+
+python3 svmKernelDual.py
